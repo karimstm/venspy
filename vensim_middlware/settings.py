67d3b032c91c2d2303242577da613b5044120275
Aresar <macuser@Aresar.local>
2019-11-26 14:12:11 +0100

    upload_update_done

--- vensim_middlware/settings.py
+++ vensim_middlware/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'coreapi',
+    'django_filters',
     'background_task',
     'corsheaders'
 ]
@@ -134,7 +135,8 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 
